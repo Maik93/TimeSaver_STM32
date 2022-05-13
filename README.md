791b72c50b3dafcda942247096f5e771b7f2a0c4
Michael Mugnai <michael.mugnai@gmail.com>
2022-05-13 17:06:33 +0200

    make leds blink! (through stm32f7xx-hal)

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ cargo install cargo-embed
 - [cortex-m-quickstart](https://github.com/rust-embedded/cortex-m-quickstart) template
 - [Cargo-embed](https://probe.rs/docs/tools/cargo-embed/) documentation
 - memory files from https://github.com/stm32-rs/stm32f7xx-hal
+- [stm32f7xx-hal examples](https://github.com/stm32-rs/stm32f7xx-hal/tree/master/examples)
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-#![no_std]
-#![no_main]
+#![no_std] // just use core Crate
+#![no_main] // manually define the function entry
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use stm32f7xx_hal as hal;
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use core::panic::PanicInfo;
+use crate::hal::{pac, prelude::*};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { loop {} }
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let dev_perip = pac::Peripherals::take().unwrap();
+
+    let gpio_b = dev_perip.GPIOB.split();
+    let mut led_1 = gpio_b.pb0.into_push_pull_output();
+    let mut led_2 = gpio_b.pb7.into_push_pull_output();
+    let mut led_3 = gpio_b.pb14.into_push_pull_output();
 
     loop {
-        // your code goes here
+        for _ in 0..10_000 {
+            led_1.set_high();
+            led_2.set_high();
+            led_3.set_high();
+        }
+
+        for _ in 0..10_000 {
+            led_1.set_low();
+            led_2.set_low();
+            led_3.set_low();
+        }
     }
 }
